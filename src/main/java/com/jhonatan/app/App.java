60642b30f43fd9dd6d0b1c8c992d363d4608cd3f
Jhonatan zuluaga <zuluagajonatan@hotmail.com>
2019-03-29 19:41:09 -0500

    datos de clase maquina

--- src/main/java/com/jhonatan/app/App.java
+++ src/main/java/com/jhonatan/app/App.java
@@ -1,10 +1,48 @@
 package com.jhonatan.app;
 
+import java.util.ArrayList;
+
+import com.jhonatan.dominio.Maquina;
+import com.jhonatan.dominio.Producto;
+
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Producto galletas = new Producto();
+	    galletas.setNombre("Festival");
+		galletas.setPrecio(800.0);
+		galletas.setCodigo(01);
+		galletas.setCantidad(12);
+		
+		Producto chocolatina = new Producto();
+		chocolatina.setNombre("jet");
+		chocolatina.setPrecio(1200.0);
+		chocolatina.setCodigo(02);
+		chocolatina.setCantidad(12);
+		ArrayList<Producto>listaProductos= new ArrayList<>();
+		listaProductos.add(galletas);
+		listaProductos.add(galletas);
+		
+		
+		
+		
+		Maquina maquinaDispensadora = new Maquina();
+		
+		
+		
+		
+		
+		
+		
+		for(int x=0;x<listaProductos.size();x++) {
+			  System.out.println(maquinaDispensadora.getProductos());
+			}
+		
+		}
+
 
 	}
 
-}
+
--- src/main/java/com/jhonatan/dominio/Maquina.java
+++ src/main/java/com/jhonatan/dominio/Maquina.java
@@ -1,5 +1,96 @@
 package com.jhonatan.dominio;
 
 public class Maquina {
+	
+
+	private String nombre;
+	private Double precio;
+	private int codigo;
+
+	private int cantidad_unidades;
+	private Producto productos;
+	
+	
+	public void sacar_producto()
+	{
+		
+		this.cantidad_unidades= cantidad_unidades;
+		this.nombre=nombre;
+		
+	}
+	
+	public void ingresar_producto(Producto productos)
+	{
+	this.productos = productos;
+		
+		
+		
+	}
+	
+	public void consultar_nombre_productoAgotado()
+	{
+		
+		
+	}
+	
+	public void consultar_totalUnidades_productoDeterminado()
+	{
+		
+		
+	}
+	
+	public void consultar_totalUnidades_todoslosProductos()
+	{
+		
+		
+	}
+
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	
+
+	
+	public int getCantidad_unidades() {
+		return cantidad_unidades;
+	}
+
+	public void setCantidad_unidades(int cantidad_unidades) {
+		this.cantidad_unidades = cantidad_unidades;
+	}
+
+	public Producto getProductos() {
+		this.productos = productos;
+		return productos;
+		
+	}
+
+	public void setProductos(Producto productos) {
+		this.productos = productos;
+	}
+
+	
 
 }
